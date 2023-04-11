2db4e3ecc6df46432e8963a28e3a6c8f6f3a2144
Iván <jivanrod@gmail.com>
2023-04-11 23:54:32 +0200

    update

--- index.php
+++ index.php
@@ -20,13 +20,9 @@ if (!empty($_POST)) {
     $errorUsuario = !esValidoNombre($usuario);
     $password1 = filter_input(INPUT_POST, 'password1', FILTER_UNSAFE_RAW);
     $password2 = filter_input(INPUT_POST, 'password2', FILTER_UNSAFE_RAW);
-    $errorPassword = !esValidoPasswords($password1, $password2);
+    $errorPasswords = !esValidoPasswords($password1, $password2);
     $email = filter_input(INPUT_POST, 'email', FILTER_UNSAFE_RAW);
     $errorEmail = !esValidoEmail($email);
-    $response = compact('errorUsuario', 'errorPassword', 'errorEmail');
-    header('Content-type: application/json');
-    echo json_encode($response);
-    die;
 }
 ?>
 <!DOCTYPE html>
@@ -51,42 +47,43 @@ if (!empty($_POST)) {
                         <h3><i class="bi bi-gear p-2"></i>Registro</h3>
                     </div>
                     <div class="card-body">
-                        <form id="registro" name='registro' novalidate>
+                        <form id="registro" name="registro" action="index.php" method="POST" novalidate>
                             <div class="input-group my-2">
                                 <span class="input-group-text"><i class="bi bi-person"></i></span>
-                                <input type="text" class="form-control {{ isset($errorUsuario) ? ($errorUsuario ? "is-invalid" : "is-valid") : "" }}"  placeholder="usuario" id='usuario' name='usuario' autofocus>
+                                <input type="text" class="form-control <?= isset($errorUsuario) ? ($errorUsuario ? "is-invalid" : "is-valid") : "" ?>"  placeholder="usuario" 
+                                       id="usuario" name="usuario" value="<?= $usuario ?? '' ?>" autofocus>
                                 <div class="invalid-feedback">
                                     Debe tener más de tres caracteres.
                                 </div>
                             </div>
                             <div class="input-group my-2">
                                 <span class="input-group-text"><i class="bi bi-key"></i></span>
-                                <input type="password" class="form-control" placeholder="contraseña" id='password1' name='password1'>
+                                <input type="password" class="form-control" placeholder="contraseña" id="password1" name="password1">
                             </div>
                             <div class="input-group my-2">
                                 <span class="input-group-text"><i class="bi bi-key"></i></span>
-                                <input type="password" class="form-control {{ isset($errorPasswords) ? ($errorPasswords ? "is-invalid" : "is-valid") : "" }}"  placeholder="Repita la contraseña" id='password2' name='password2'>
+                                <input type="password" class="form-control <?= isset($errorPasswords) ? ($errorPasswords ? "is-invalid" : "is-valid") : "" ?>"  
+                                       placeholder="Repita la contraseña" id="password2" name="password2">
                                 <div class="invalid-feedback">
                                     Deben tener más de 5 caracteres o ser iguales.
                                 </div>
                             </div>
                             <div class="input-group my-2">
                                 <span class="input-group-text"><i class="bi bi-envelope"></i></span>
-                                <input type="email" class="form-control {{ isset($errorEmail) ? ($errorEmail ? "is-invalid" : "is-valid") : "" }}" placeholder="e-Mail" name='email' id='email'> 
+                                <input type="email" class="form-control <?= isset($errorEmail) ? ($errorEmail ? "is-invalid" : "is-valid") : "" ?>" 
+                                       placeholder="e-Mail" name="email" id="email" value="<?= $email ?? '' ?>"> 
                                 <div class="invalid-feedback">
                                     La dirección de email NO es válida.
                                 </div>
                             </div>
                             <div class="text-end">
-                                <input type="submit" value="Registrar" class="btn btn-info" name='enviar'>
+                                <input type="submit" value="Registrar" class="btn btn-info" name="enviar">
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-        <script src="validar.js"></script>
     </body>
 </html>

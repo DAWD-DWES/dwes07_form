bf549ac9c6805bff923971188d020678a4320407
Iván <jivanrod@gmail.com>
2024-04-15 14:53:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -34,13 +34,13 @@
                                 <div class="input-group my-2">
                                     <span class="input-group-text"><i class="bi bi-key"></i></span>
                                     <input type="password" class="form-control" placeholder="contraseña" 
-                                           id="password1" name="password1" minlength="5" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}">
+                                           id="password1" name="password1" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}">
                                     
                                 </div>
                                 <div class="input-group my-2">
                                     <span class="input-group-text"><i class="bi bi-key"></i></span>
                                     <input type="password" class="form-control"  placeholder="Repita la contraseña" 
-                                           id="password2" name="password2" minlength="5" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}">
+                                           id="password2" name="password2" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}">
                                     <div class="invalid-feedback">
                                         El password debe tener una minúscula, mayúscula, digito y caracter espercial
                                     </div>

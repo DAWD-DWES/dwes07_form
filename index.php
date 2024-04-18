3a5511720be1c3ee3e499f6bf8b593faee5ba6a1
Iván <jivanrod@gmail.com>
2024-04-18 17:39:32 +0200

    fix

--- index.php
+++ index.php
@@ -1,41 +1,45 @@
 <?php
+define("RETRO_NOMBRE_FORMATO", "El nombre de estar formado por al menos 3 caracteres de palabra");
+define("RETRO_EMAIL_FORMATO", "El correo debe tener un formato correcto");
+define("RETRO_PASS_NO_REPETIDO", "Los passwords introducidos deben de ser iguales");
+define("RETRO_PASS_FORMATO", "El password debe tener una minúscula, mayúscula, digito y caracter espercial");
 
-// El campo nombre debe tener más de 3 caracteres
-function esValidoNombre(string $nombre): bool {
-    return (strlen($nombre) > 3);
-}
-
-// El campo correo debe tener el formato adecuado
-function esValidoPassword(string $pass): bool {
-    return (strlen($pass) > 5);
-}
-
-// El campo correo debe tener el formato adecuado
-function esValidoEmail(string $email): bool {
-    return preg_match("/^[a-z0-9]+([_\\.-][a-z0-9]+)*@([a-z0-9]+([\.-][a-z0-9]+)*)+\\.[a-z]{2,}$/i", $email);
-}
-
-// Las contraseñas deben de  coincidir
-function esValidoPasswords(string $pass1, string $pass2): bool {
-    return ($pass1 == $pass2) && (strlen($pass1) > 5);
-}
-
-if (filter_has_var(INPUT_POST, 'enviar')) {
+if (!empty($_POST)) {
     $usuario = filter_input(INPUT_POST, 'usuario', FILTER_UNSAFE_RAW);
-    $errorUsuario = !esValidoNombre($usuario);
+    $errorUsuarioFormato = (filter_var($usuario, FILTER_VALIDATE_REGEXP, ["options" => [
+                    "regexp" => "/^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ'´`-]+(\s+[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ'´`-]+){0,5}$/"]]) === false);
     $password1 = filter_input(INPUT_POST, 'password1', FILTER_UNSAFE_RAW);
-    $errorPassword1 = !esValidoPassword($password1);
     $password2 = filter_input(INPUT_POST, 'password2', FILTER_UNSAFE_RAW);
-    $errorPasswords = !esValidoPasswords($password1, $password2);
-    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-    $errorEmail = !esValidoEmail($email);
-
-    $response = compact('errorUsuario', 'errorPassword1', 'errorPasswords', 'errorEmail');
-    header('Content-type: application/json');
-    echo (json_encode($response));
-    die;
+    $errorPasswordNoRepetido = ($password1 !== $password2);
+    $errorPasswordFormato = (filter_var($password1, FILTER_VALIDATE_REGEXP, ["options" => [
+                    "regexp" => "/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}/"]]) === false);
+    $email = filter_input(INPUT_POST, 'email', FILTER_UNSAFE_RAW);
+    $errorEmailFormato = (filter_var($email, FILTER_VALIDATE_REGEXP, ["options" => [
+                    "regexp" => "/^[a-z0-9]+([_\\.-][a-z0-9]+)*@([a-z0-9]+([\.-][a-z0-9]+)*)+\\.[a-z]{2,}$/i"]]) === false);
+    $errors = [];
+    if ($errorUsuarioFormato) {
+        $errors['usuario'] = RETRO_NOMBRE_FORMATO;
+    }
+    if ($errorPasswordNoRepetido) {
+        $errors['password2'] = RETRO_PASS_NO_REPETIDO;
+    } else if ($errorPasswordFormato) {
+        $errors['password2'] = RETRO_PASS_FORMATO;
+    }
+    if ($errorEmailFormato) {
+        $errors['email'] = RETRO_EMAIL_FORMATO;
+    }
+    if (filter_has_var(INPUT_POST, 'enviar')) {
+        $response['success'] = empty($errors);
+        $response['errors'] = $errors;
+        header('Content-type: application/json');
+        echo json_encode($response);
+        die;
+    } else {
+        $procesa = true;
+    }
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
     <head>
@@ -49,61 +53,54 @@ if (filter_has_var(INPUT_POST, 'enviar')) {
     </head>
     <body class="bg-info">
         <div class="container mt-5">
-            <?php if (!empty($_POST) && !isset($_POST['enviar'])): ?>
+            <?php if (isset($procesa)): ?>
                 <div class="alert alert-success" id="mensaje" role="alert">
                     Registro realizado con éxito
                 </div>
-            <?php else: ?>
-                <div class="d-flex justify-content-center h-100">
-                    <div class="card w-50">
-                        <div class="card-header">
-                            <h3><i class="bi bi-gear p-2"></i>Registro</h3>
-                        </div>
-                        <div class="card-body">
-                            <form id="registro" name="registro" method="POST" novalidate>
-                                <div class="input-group my-2">
-                                    <span class="input-group-text"><i class="bi bi-person"></i></span>
-                                    <input type="text" class="form-control"  placeholder="usuario" 
-                                           id="usuario" name="usuario" autofocus>
-                                    <div class="invalid-feedback">
-                                        Debe tener más de tres caracteres.
-                                    </div>
+            <?php endif ?>
+            <div class="d-flex justify-content-center h-100">
+                <div class="card w-50">
+                    <div class="card-header">
+                        <h3><i class="bi bi-gear p-2"></i>Registro</h3>
+                    </div>
+                    <div class="card-body">
+                        <form id="registro" name="registro" action="index.php" method="POST" novalidate>
+                            <div class="input-group my-2">
+                                <span class="input-group-text"><i class="bi bi-person"></i></span>
+                                <input type="text" class="form-control"  placeholder="usuario" 
+                                       id="usuario" name="usuario" value="<?= $usuario ?? '' ?>" autofocus>
+                                <div class="invalid-feedback">
                                 </div>
-                                <div class="input-group my-2">
-                                    <span class="input-group-text"><i class="bi bi-key"></i></span>
-                                    <input type="password" class="form-control" placeholder="contraseña" 
-                                           id="password1" name="password1">
-                                    <div class="invalid-feedback">
-                                        Deben tener más de cinco caracteres.
-                                    </div>
+                            </div>
+                            <div class="input-group my-2">
+                                <span class="input-group-text"><i class="bi bi-key"></i></span>
+                                <input type="password" class="form-control" placeholder="contraseña" id="password1" name="password1" 
+                                       value="<?= $password1 ?? '' ?>">
+                            </div>
+                            <div class="input-group my-2">
+                                <span class="input-group-text"><i class="bi bi-key"></i></span>
+                                <input type="password" class="form-control"  
+                                       placeholder="Repita la contraseña" id="password2" name="password2" value="<?= $password2 ?? '' ?>">
+                                <div class="invalid-feedback">
                                 </div>
-                                <div class="input-group my-2">
-                                    <span class="input-group-text"><i class="bi bi-key"></i></span>
-                                    <input type="password" class="form-control"  placeholder="Repita la contraseña" 
-                                           id="password2" name="password2">
-                                    <div class="invalid-feedback">
-                                        Deben tener más de 5 caracteres o ser iguales.
-                                    </div>
+                            </div>
+                            <div class="input-group my-2">
+                                <span class="input-group-text"><i class="bi bi-envelope"></i></span>
+                                <input type="email" class="form-control"
+                                       placeholder="e-Mail" name="email" id="email" value="<?= $email ?? '' ?>">
+                                <div class="invalid-feedback">
                                 </div>
-                                <div class="input-group my-2">
-                                    <span class="input-group-text"><i class="bi bi-envelope"></i></span>
-                                    <input type="email" class="form-control" placeholder="e-Mail" 
-                                           id="email" name="email"> 
-                                    <div class="invalid-feedback">
-                                        La dirección de email NO es válida.
-                                    </div>
-                                </div>
-                                <div class="text-end">
-                                    <input type="submit" id="enviar" value="Registrar" class="btn btn-info" name="enviar">
-                                </div>
-                            </form>
-                        </div>
+                            </div>
+                            <div class="text-end">
+                                <input type="submit" value="Registrar" class="btn btn-info" name="enviar">
+                            </div>
+                        </form>
                     </div>
                 </div>
-            <?php endif ?>
+            </div>
         </div>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity = "sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin = "anonymous"></script>
         <script src="validar.js"></script>
     </body>
 </html>
--- validar.js
+++ validar.js
@@ -1,43 +1,94 @@
 $(document).ready(function () {
-    $("#registro").submit(validaForm);
+    $('#registro').on('submit', function (e) {
+        e.preventDefault();
+        var form = this;
+
+        // Usar el método de jQuery para crear el objeto FormData
+        var formData = new FormData(form);
+        formData.append($(form).find(':submit').attr('name'), $(form).find(':submit').val());
+
+        // Usar jQuery.ajax() para enviar los datos
+        $.ajax({
+            url: 'index.php',
+            type: 'POST',
+            data: formData,
+            contentType: false, // importante para enviar datos de tipo FormData
+            processData: false, // importante para enviar datos de tipo FormData
+            dataType: 'json', // Esperamos JSON de vuelta
+            success: function (response) {
+                $(form).removeClass('was-validated');
+
+                // Ajustar la validación en cada campo específico
+                $.each(form.elements, function () {
+                    var input = this;
+                    var feedback = $(input).next('.invalid-feedback');
+                    if (response.errors[input.name]) {
+                        $(feedback).text(response.errors[input.name]);
+                        $(feedback).show();
+                        $(input).addClass('is-invalid').removeClass('is-valid');
+                    } else {
+                        $(input).removeClass('is-invalid').addClass('is-valid');
+                        $(feedback).text('').hide();
+                    }
+                });
+
+                if (response.success) {
+                    // Si quieres hacer algo al tener éxito, como redireccionar o mostrar un mensaje
+                    // alert('Registro completado con éxito.');
+                    form.submit(); // O puedes redireccionar usando window.location.href a otra URL
+                    setTimeout(() => {
+                        form.reset(); // Intenta resetear el formulario después de un breve retraso
+                    }, 100);
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error('Error en la red o en el servidor: ' + textStatus + ', ' + errorThrown);
+            }
+        });
+    });
 });
 
-function validaForm(e) {
-    e.preventDefault();
-    e.stopImmediatePropagation();
-    const form = e.target;
-    $.ajax({
-        type: "POST",
-        url: 'index.php',
-        data: $(this).serialize() + `&${form.enviar.name}=${form.enviar.value}`,
-        context: form,
-        dataType: "json",
-        success: function (response)
-        {
-            this.usuario.setCustomValidity("");
-            this.password1.setCustomValidity("");
-            this.password2.setCustomValidity("");
-            this.email.setCustomValidity("");
-            if (!(response.errorUsuario || response.errorPassword || response.errorEmail)) {
-                this.submit();
-            } else {
-                if (response.errorUsuario) {
-                    this.usuario.setCustomValidity("error");
-                }
-                if (response.errorPassword1) {
-                    this.password1.setCustomValidity("error");
-                }
-                if (response.errorPasswords) {
-                    this.password2.setCustomValidity("error");
-                }
-                if (response.errorEmail) {
-                    this.email.setCustomValidity("error");
-                }
-            }
-            form.classList.add('was-validated');
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            alert('Error Message: ' + thrownError);
-        }
-    });
-}
+
+/* $(document).ready(function () {
+ $("#registro").submit(validaForm);
+ });
+ 
+ function validaForm(e) {
+ e.preventDefault();
+ e.stopImmediatePropagation();
+ const form = e.target;
+ $.ajax({
+ type: "POST",
+ url: 'index.php',
+ data: $(this).serialize() + `&${form.enviar.name}=${form.enviar.value}`,
+ context: form,
+ dataType: "json",
+ success: function (response)
+ {
+ this.usuario.setCustomValidity("");
+ this.password1.setCustomValidity("");
+ this.password2.setCustomValidity("");
+ this.email.setCustomValidity("");
+ if (!(response.errorUsuario || response.errorPassword || response.errorEmail)) {
+ this.submit();
+ } else {
+ if (response.errorUsuario) {
+ this.usuario.setCustomValidity("error");
+ }
+ if (response.errorPassword1) {
+ this.password1.setCustomValidity("error");
+ }
+ if (response.errorPasswords) {
+ this.password2.setCustomValidity("error");
+ }
+ if (response.errorEmail) {
+ this.email.setCustomValidity("error");
+ }
+ }
+ form.classList.add('was-validated');
+ },
+ error: function (xhr, ajaxOptions, thrownError) {
+ alert('Error Message: ' + thrownError);
+ }
+ });
+ } */

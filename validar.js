33a37a07eafefde777796e5da0c817ae74e37fa1
Iván <jivanrod@gmail.com>
2024-04-18 16:35:09 +0200

    Update validar.js

--- validar.js
+++ validar.js
@@ -13,19 +13,15 @@ function validaForm(e) {
     const errPassword = password1.value !== password2.value;
     if (errPassword) {
         // Establece el mensaje de error y evita el envío del formulario
-        password2.setCustomValidity("Los passwords introducidos deben de ser iguales");
         password2.classList.add('is-invalid');
         errorPassword2.textContent = password2.validationMessage; // Asegúrate de que el mensaje sea el personalizado
+        errorPassword2.textContent = "Los passwords introducidos deben de ser iguales";
         e.preventDefault();
         e.stopImmediatePropagation();
-    } else {
-        password2.setCustomValidity("");
     }
-
     if (!form.checkValidity()) {
         e.preventDefault();
         e.stopImmediatePropagation();
     }
     form.classList.add('was-validated');
 }
-
